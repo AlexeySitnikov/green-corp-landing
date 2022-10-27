b4e231327dfb18848525f2f5da29be64c8ba9403
AlexeySitnikov <alexey.sitnikov@bk.ru>
2022-10-27 23:39:14 +0300

    Проектное задание 3.6. is completed

--- js/animations.js
+++ js/animations.js
@@ -2,11 +2,19 @@ const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
   if (event.target.value === 'other') {
-    // Должны добавить еще одно текстовое поле
+    const formContainer = document.createElement("div");
+    formContainer.classList.add("form__group");
+    formContainer.classList.add("form__other-input");
+    const input = document.createElement("input");
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text";
+    formContainer.appendChild(input);   
+    document.querySelector(".form form").insertBefore(formContainer,document.querySelector(".form__submit"));
   }
-
-  if (event.target.value !== 'other') {
-    // Удаляем ранее добавленное текстовое поле, если оно есть в DOM
+  
+  const otherInput = document.querySelector(".form__other-input");
+  if (event.target.value !== 'other' && otherInput) {       
+    document.querySelector(".form form").removeChild(otherInput);
   }
 });
 
